bba0cbd44182740ba8e99e624e0600770f11f05d
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2021-01-27 23:41:34 -0300

    Iniciando no JavaScript

---
+++ js/main.js
@@ -0,0 +1,78 @@
+
+var d = new Date();
+//alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count)
+    count = count + 1; //count ++ é a mesma coisa que count = count + 1//
+    
+}
+*/
+
+
+/*
+var idade = prompt("Qual a sua idade?")
+
+//var idade = 18;
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+console.log(frutas.nome);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor)
+*/
+
+
+//var lista = ["maça", "pêra", "laranja"]
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista[0]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.toString()[0])
+//console.log(lista.join(" + "))
+
+
+//var nome = "Luiz Fernando";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
