6ffd4b36003fa70c187568f9e6cbb6c0d18062c6
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2021-01-29 00:12:09 -0300

    manipulando elementos da página com function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,77 @@
 
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>";
+    //console.log(document.getElementById("agradecimento"));
+   // alert("Obrigado por clicar!!!");
+}
+
+function redirecionar(){
+    window.open("https://github.com/luizfernandoliveira/DIO_aula_javascript");
+    //window.location.href = "https://github.com/luizfernandoliveira/DIO_aula_javascript";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!";
+    elemento.innerHTML = "Obrigado por passar o mouse!";
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "passe o mouse aqui!";
+    elemento.innerHTML = "passe o mouse aqui!";
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2
+}
+
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+
+*/
+
+//alert(soma(5, 10));
+
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10))
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+
+
+
+/*
 var d = new Date();
 //alert(d);
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
 
+*/
 
 /*
 var count;
